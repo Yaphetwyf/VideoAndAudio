cb6671c266ff681ff8466a2c99bcbc6cfb20e8a3
wyf <635272824@qq.com>
2021-02-19 22:46:25 +0800

    1.增加cpp文件以及cmake的基本使用

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,3 +2,14 @@
 // Created by 62527 on 2021/2/18.
 //
 
+#include <jni.h>
+#include <string>
+#include "people/people.h"
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_example_wyf_avdio_MainActivity_stringFromJNI(JNIEnv *env, jobject thiz) {
+    // TODO: implement stringFromJNI()
+    people mpeople;
+    env->NewStringUTF(mpeople.getString().c_str());
+}
